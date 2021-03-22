78e7837d1b04bf5eb8b81d024fd3238a713b46af
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-22 19:29:04 +0100

    change value of conta milili array

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+const milili = [10, 25, 4];
+
+function myArr(mili) {
+    "use strict";
+
+    // Only change code below this line
+    milili[0] = 4;
+    milili[1] = 10;
+    milili[2] = 25;
+
+
+    // Only change code above this line
+    return mili;
+}
+console.log(myArr(milili));
+module.exports = myArr;
